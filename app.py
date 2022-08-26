61ffc597a573f19bb2f6ffd40a03f407ecc50064
antaares <jrismoilov@gmail.com>
2022-08-26 16:06:53 +0500

    update aiogram version

--- app.py
+++ app.py
@@ -2,16 +2,10 @@ from aiogram import executor
 
 from loader import dp
 import middlewares, filters, handlers
-from utils.notify_admins import on_startup_notify
-from utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
-    # Birlamchi komandalar (/star va /help)
-    await set_default_commands(dispatcher)
-
-    # Bot ishga tushgani haqida adminga xabar berish
-    await on_startup_notify(dispatcher)
+    pass
 
 
 if __name__ == '__main__':
